1d4dfb7cf72f18527116240b03bbcb53c5dee6d4
EddyBautista <bautista.j.eddy@gmail.com>
2020-12-06 12:28:12 -0600

    Movies page with CRUD.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -8,9 +8,14 @@ namespace RazorPagesMovie.Models
 {
     public class Movie
     {
+        // id always required for primary key
         public int ID { get; set; }
         public string Title { get; set; }
 
+        // specifies the datatype with the attribute
+        //The user isn't required to enter time information in the date field.
+        //Only the date is displayed, not time information.
+        
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
